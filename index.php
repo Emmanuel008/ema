e51b4d9c0b022e1f50483f3cb46f787524ccf7ab
emmanuel008 <emmanueljeremiah008@gmail.com>
2024-07-03 03:17:08 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,396 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>iBUA Innovation Hub</title>
+
+        <!-- CSS FILES -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-icons.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/slick.css"/>
+        <link rel="stylesheet" href="css/testimonial.css"/>
+        <link rel="stylesheet" href="css/partner.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/tooplate-little-fashion.css" rel="stylesheet">
+<!--
+
+Tooplate 2127 Little Fashion
+
+https://www.tooplate.com/view/2127-little-fashion
+
+-->
+    </head>
+    
+    <body>
+
+        <section class="preloader">
+            <div class="spinner">
+                <span class="sk-inner-circle"></span>
+            </div>
+        </section>
+    
+        <main>
+
+            <nav class="navbar navbar-expand-lg">
+                <div class="container">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <a class="navbar-brand" href="index.html">
+                        <strong><span>iBUA</span> Hub</strong>
+                    </a>
+
+                    <div class="d-lg-none">
+                        <a href="sign-in.html" class="bi-person custom-icon me-3"></a>
+
+                        <a href="product-detail.html" class="bi-bag custom-icon"></a>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="index.php">Home</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.php">About</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link active" href="Updates.php">Updates</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="faq.php">FAQs</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.html">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <section class="slick-slideshow">   
+                <div class="slick-custom">
+                    <img src="images/COSTECH-15 - Copy.jpg" class="img-fluid" alt="">
+
+                    <div class="slick-bottom">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6 col-10">
+                                    <h1 class="slick-title">iBUA Innovation Hub</h1>
+
+                                    <p class="lead text-white mt-lg-3 mb-lg-5"><strong>iBUA Innovation Hub is a dynamic initiative in Maruhubi, Zanzibar, fostering innovation and entrepreneurship.</strong>
+                                        </p>
+
+                                    <a href="about.html" class="btn custom-btn">Learn more about us</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="slick-custom">
+                    <img src="images/COSTECH-17 - Copy.jpg" class="img-fluid" alt="">
+
+                    <div class="slick-bottom">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6 col-10">
+                                    <h1 class="slick-title">iBUA Hub</h1>
+
+                                    <p class="lead text-white mt-lg-3 mb-lg-5"><strong>iBUA is a catalyst for change, promoting creativity, entrepreneurship, and sustainable development in Zanzibar and beyond.</strong></p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="slick-custom">
+                    <img src="images/COSTECH-10 - Copy.jpg" class="img-fluid" alt="">
+
+                    <div class="slick-bottom">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6 col-10">
+                                    <h1 class="slick-title">Strength Innovation in Zanzibar</h1>
+
+                                    <p class="lead text-white mt-lg-3 mb-lg-5"><strong>iBUA aims to strengthen Zanzibar's innovation ecosystem by creating a collaborative community across sectors.</strong></p>
+
+                                    <a href="about.html" class="btn custom-btn">learn more</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+            <section id="carouselExampleIndicators" class="carousel slide section-padding" data-bs-ride="carousel">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2 class="mb-5">Latest <span> Updates</span></h2>
+            </div>
+            <div class="col-lg-12 col-12">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="row">
+                            <div class="col-lg-7 col-12">
+                                <img src="images/saba.jpeg" class="img-fluid mh-100" alt="">
+                            </div>
+                            <div class="col-lg-5 col-12">
+                                <div class="d-flex flex-column h-100 ms-lg-4 mt-lg-0 mt-5">
+                                    <h4 class="mb-3">iBUA <span>Hub</span> <br>Talent <span>Pool</span> Program</h4>
+                                    <p>Specialized initiative designed to identify, nurture, and develop exceptional talent in various fields within the Zanzibar innovation ecosystem.</p>
+                                    <div class="mt-2 mt-lg-auto">
+                                        <a href="about.html" class="custom-link mb-2">
+                                            Read more 
+                                            <i class="bi-arrow-right ms-2"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row">
+                            <div class="col-lg-7 col-12">
+                                <img src="images/COSTECH-19 - Copy.jpg" class="img-fluid mh-100" alt="">
+                            </div>
+                            <div class="col-lg-5 col-12">
+                                <div class="d-flex flex-column h-100 ms-lg-4 mt-lg-0 mt-5">
+                                    <h4 class="mb-3">iBUA <span>Hub</span> <br>Management <span>training</span> Support</h4>
+                                    <p>Aims to equip entrepreneurs, innovators, and business leaders with the skills, knowledge, and tools necessary to effectively manage and grow their ventures.</p>
+                                    <div class="mt-2 mt-lg-auto">
+                                        <a href="about.html" class="custom-link mb-2">
+                                            Read more 
+                                            <i class="bi-arrow-right ms-2"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="p-5 align-items-center justify-content-center d-flex"> 
+    <button type="button" class="btn text-white" style="background-color: #FF4400"><a href="Updates.html" class="View More text-white">View More</a></button>
+</section>
+
+
+
+                       
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+					<div class="sec-heading text-center">
+						<h6>Testimonial</h6>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="clients-carousel owl-carousel">
+					<div class="single-box">
+						<div class="img-area"><img alt="" class="img-fluid" src="images/COSTECH-13 - Copy.jpg"></div>
+						<div class="content">
+							<p>"iBUA Hub imenishika mkono kunisaidia katika hatua zangu za kibunifu."</p>
+							<h4>Sharmila Hassan</h4>
+							<h6>Mbunifu</h6>
+						</div>
+					</div>
+					<div class="single-box">
+						<div class="img-area"><img alt="" class="img-fluid" src="images/ima.jpeg"></div>
+						<div class="content">
+							<p>"Naishukuru serikali kupitia iBUA kwa ufuatiliaji na utekelezaji wenye matokeo chanya ."</p>
+							<h4>Emmanuel Jeremy</h4>
+							<h6>Mbunifu</h6>
+						</div>
+					</div>
+					<div class="single-box">
+						<div class="img-area"><img alt="" class="img-fluid" src="https://images.pexels.com/photos/3211476/pexels-photo-3211476.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></div>
+						<div class="content">
+							<p>"Naishukuru serikali kupitia iBUA kwa ufuatiliaji na utekelezaji wenye matokeo chanya."</p>
+							<h4>Hamisa Juma</h4>
+							<h6>Mbunifu</h6>
+						</div>
+					</div>
+					<div class="single-box">
+						<div class="img-area"><img alt="" class="img-fluid" src="images/gee.jpeg"></div>
+						<div class="content">
+							<span class="rating-star"><i class="icofont-star"></i><i class="icofont-star"></i><i class="icofont-star"></i><i class="icofont-star"></i><i class="icofont-star"></i></span>
+							<p>"iBUA Hub imenishika mkono kunisaidia katika hatua zangu za kibunifu.."</p>
+							<h4>Amirat Omary</h4>
+							<h6>Mbunifu</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+    <div class="container">
+        <h2>Our Partners </h2>
+         <section class="customer-logos slider">
+            <div class="slide"><img src="images/costech.png"></div>
+            <div class="slide"><img src="images/zpc.jpeg"></div>
+            <div class="slide"><img src="images/buni.png"></div>
+            <div class="slide"><img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
+
+         </section>
+    
+
+	
+
+                    </div>
+                </div>
+            </section>
+
+        </main>
+
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-3 col-10 me-auto mb-4">
+                        <h4 class="text-white mb-3"><a href="index.html">iBUA</a> Hub</h4>
+                        <p class="copyright-text text-muted mt-lg-5 mb-4 mb-lg-0">Copyright © 2024 <strong>iBUA</strong></p>
+                        <br>
+                        <p class="copyright-text"><a href="https://www.tooplate.com/" target="_blank"></a></p>
+                    </div>
+
+                    <div class="col-lg-5 col-8">
+                        <h5 class="text-white mb-3">Sitemap</h5>
+
+                        <ul class="footer-menu d-flex flex-wrap">
+                            <li class="footer-menu-item"><a href="about.html" class="footer-menu-link">About</a></li>
+
+                            <li class="footer-menu-item"><a href="#" class="footer-menu-link">Updates</a></li>
+
+                            <li class="footer-menu-item"><a href="#" class="footer-menu-link">Privacy policy</a></li>
+
+                            <li class="footer-menu-item"><a href="#" class="footer-menu-link">FAQs</a></li>
+
+                            <li class="footer-menu-item"><a href="#" class="footer-menu-link">Contact</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-4">
+                        <h5 class="text-white mb-3">Social</h5>
+
+                        <ul class="social-icon">
+
+                            <li><a href="#" class="social-icon-link bi-youtube"></a></li>
+
+                            <li><a href="#" class="social-icon-link bi-whatsapp"></a></li>
+
+                            <li><a href="#" class="social-icon-link bi-instagram"></a></li>
+
+                            <li><a href="#" class="social-icon-link bi-skype"></a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+
+        <!-- JAVASCRIPT FILES -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/Headroom.js"></script>
+        <script src="js/jQuery.headroom.js"></script>
+        <script src="js/slick.min.js"></script>
+        <script src="js/custom.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+        </script> 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js">
+        </script> 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js">
+        </script> 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script>
+               $('.clients-carousel').owlCarousel({
+                   loop: true,
+                   nav: false,
+                   autoplay: true,
+                   autoplayTimeout: 5000,
+                   animateOut: 'fadeOut',
+                   animateIn: 'fadeIn',
+                   smartSpeed: 450,
+                   margin: 30,
+                   responsive: {
+                       0: {
+                           items: 1
+                       },
+                       768: {
+                           items: 2
+                       },
+                       991: {
+                           items: 3
+                       },
+                       1200: {
+                           items: 3
+                       },
+                       1920: {
+                           items: 3
+                       }
+                   }
+               });
+        </script>
+        <script>
+            $(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+        </script>
+
+    </body>
+</html>
